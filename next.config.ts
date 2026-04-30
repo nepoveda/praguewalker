64d4c57239518cac17cee817313fe50eb7048683
Dan Nepejchal <dan.nepejchal@dhl.com>
2026-04-30 19:45:42 +0200

    Fix metadata, config for build

--- next.config.ts
+++ next.config.ts
@@ -3,19 +3,22 @@ import type {NextConfig} from "next";
 const nextConfig: NextConfig = {
   images: {
     // This will make every image lazy‑loaded by default
-    unoptimized: false,
+    unoptimized: true,
     loader: 'default',
     remotePatterns: [
       {
         protocol: "https",
         hostname: "hoirqrkdgbmvpwutwuwj.supabase.co",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "images.unsplash.com",
+        pathname: "/**",
       },
     ],
   },
+  output: "export",
 };
 
 export default nextConfig;
